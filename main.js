5b36d613b16acc7738c2fcfb0943c5b13a47ec5d
Greg Berisha <gregberisha@intern.local>
2019-04-17 10:03:53 +0100

    adding pages

---
+++ main.js
@@ -0,0 +1,59 @@
+const carouselSlide = document.querySelector('.carousel-slide');
+const carouselImages = document.querySelectorAll('.carousel-slide img');
+
+//Buttons
+const prevBtn = document.querySelector('#prevBtn');
+const nextBtn = document.querySelector('#nextBtn');
+
+//Counter
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+
+//Button Listeners
+
+nextBtn.addEventListener('click', () => {
+    if(counter >= carouselImages.length -1) return;  
+    carouselSlide.style.transition = "transform 0.5s ease-in-out";
+    counter++;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    
+});
+
+prevBtn.addEventListener('click', () => {
+    if (counter <= 0) return;
+    carouselSlide.style.transition = "transform 0.5s ease-in-out";
+    counter--;
+    carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    
+});
+
+carouselSlide.addEventListener('transitionend', () => {
+    if (carouselImages[counter].id === 'lastClone'){
+      carouselSlide.style.transition = "none";  
+      counter = carouselImages.length -2 ; 
+      carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if (carouselImages[counter].id === 'firstClone'){
+        carouselSlide.style.transition = "none";  
+        counter = carouselImages.length - counter ; 
+        carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+      }
+});
+
+
+function openTab(tabName) {
+  var i, x;
+  x = document.getElementsByClassName("containerTab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(tabName).style.display = "block";
+}
+var submitButton = document.getElementById('form-submit-button');
+submitButton.addEventListener('click', function(event){
+  event.preventDefault();
+
+  window.alert('Thanks for placing your order, we will contact you shortly.  kind regards, BMW.')
+})
